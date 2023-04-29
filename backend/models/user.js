681301be894b6eaba7e62a27351339aae794ab62
Ishaan Jamdar <ishaan.jamdar@gmail.com>
2023-04-29 12:38:29 -0400

    finish user schema for now

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,3 +1,29 @@
 const mongoose = require('mongoose');
 
+const url = process.env.url;
 
+console.log('connecting to', url);
+mongoose.connect(url)
+  .then(console.log('connected'))
+  .catch(err => {
+    console.log('error connecting:', err.message);
+  })
+
+const userSchema = new mongoose.Schema({
+  userName: String,
+  password: String,
+  car: Boolean,
+  lat: Number,
+  long: Number, 
+  phoneNumber: String
+})
+
+userSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  }
+})
+
+module.exports = mongoose.model('User', userSchema);
